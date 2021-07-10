09457e35663f4c49279db0fc37e8e0774eed237b
Ratakondala Arun <ratakondalaarun@gmail.com>
2021-07-10 22:03:42 +0530

    🔥 removed duplicate test assets

--- test/web/script/index.js
+++
@@ -1,8 +0,0 @@
-window.onload = () => {
-  document.getElementById("js-file-cb").checked = true;
-  document.getElementById("js-file-cb").disabled = true;
-  document.getElementById("css-file-cb").checked = true;
-  document.getElementById("css-file-cb").disabled = true;
-  document.getElementById("html-file-cb").checked = true;
-  document.getElementById("html-file-cb").disabled = true;
-};
